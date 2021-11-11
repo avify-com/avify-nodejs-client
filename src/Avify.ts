52e38140d9988f1e8b2900575e1905b379de046e
Alexis Valenciano <gabr.933@gmail.com>
2021-11-10 19:42:44 -0600

    Encryp cards stable release

--- src/Avify.ts
+++ src/Avify.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import handleError from './functions/handleError';
+
 export interface ApiResponse<T> {
   success: boolean;
   httpCode: number;
@@ -12,18 +13,27 @@ export interface ApiResponse<T> {
 export interface GetPublicKeyResponse {
   key: string;
 }
-type Mode = 'sandbox' | 'production';
+export type Mode = 'sandbox' | 'production';
 type Version = 'v1';
-interface Constructor {
+export interface Constructor {
   mode: Mode;
   version: Version;
 }
 
+export interface CheckoutOptions {
+  currency: 'USD' | 'CRC';
+  cardHolder: string;
+  cardNumber: string;
+  cvc: string;
+  expMonth: string;
+  expYear: string;
+}
 class Avify {
   public prodBaseUrl = 'https://api.avify.co';
   public sadboxBaseUrl = 'https://sandboxapi.avify.co';
   public baseUrl: string;
   public mode: string;
+  public publicKey: string | undefined;
   constructor({ mode, version }: Constructor) {
     this.mode = mode;
     this.baseUrl =
@@ -32,6 +42,15 @@ class Avify {
       version;
   }
   async getPublicKey(): Promise<ApiResponse<GetPublicKeyResponse>> {
+    if (this.publicKey) {
+      return {
+        data: {
+          key: this.publicKey
+        },
+        success: true,
+        httpCode: 200
+      };
+    }
     const response = await axios
       .get(this.baseUrl + '/gateway/key', {
         timeout: 10000
@@ -43,12 +62,91 @@ class Avify {
     if (error) {
       return error;
     }
+    this.publicKey = response.data.key;
     return {
       data: {
         key: response.data.key
       },
       success: true,
       httpCode: response.status
+    };
+  }
+  async encrypt(text: string): Promise<ApiResponse<string>> {
+    const pubKey = await this.getPublicKey();
+    if (!pubKey.success) {
+      return {
+        httpCode: pubKey.httpCode,
+        success: false,
+        error: pubKey.error
+      };
+    }
+
+    // tslint:disable-next-line: prefer-const
+    if (typeof window === 'undefined') {
+      const crypto = require('crypto');
+      const buf = Buffer.from(text, 'utf8');
+
+      // Encrypting the text
+      const secretData = crypto.publicEncrypt(
+        {
+          key: pubKey.data?.key || 'nothing',
+          padding: crypto.constants.RSA_PKCS1_PADDING
+        },
+        buf
+      );
+      // printing the encrypted text
+
+      return {
+        data: secretData.toString('base64'),
+        success: true,
+        httpCode: 200
+      };
+    }
+    // tslint:disable-next-line: variable-name
+    const JSEncrypt = require('jsencrypt');
+    const jsencrypt = new JSEncrypt();
+    jsencrypt.setPublicKey(pubKey.data?.key || 'nothing');
+    const secret = jsencrypt.encrypt(text);
+    if (!secret) {
+      return {
+        error: {
+          message: 'Error al intentar encriptar el mensaje',
+          code: 'G-000'
+        },
+        success: false,
+        httpCode: 500
+      };
+    }
+    return {
+      data: secret,
+      success: true,
+      httpCode: 200
+    };
+  }
+  async checkout(options: CheckoutOptions): Promise<ApiResponse<any>> {
+    const encryptedCard = await this.encrypt(JSON.stringify(options));
+    const response = await axios
+      .post(
+        this.baseUrl + '/gateway/checkout',
+        {
+          card: encryptedCard.data
+        },
+        {
+          timeout: 10000
+        }
+      )
+      .catch((err) => {
+        return err;
+      });
+    const error = handleError(response);
+    if (error) {
+      return error;
+    }
+    const data = response.data;
+    return {
+      data,
+      success: true,
+      httpCode: 200
     };
   }
 }
--- test/index.js
+++ test/index.js
@@ -3,7 +3,13 @@ const Avify = require('../lib/index');
 const avify = new Avify({ mode: 'sandbox', version: 'v1' });
 
 const start = async () => {
-  const pub = await avify.getPublicKey();
-  console.log(pub);
+  const data = await avify.checkout({
+    cardHolder: 'Alexis Valenciano',
+    cvc: '248',
+    expMonth: '06',
+    expYear: '2023',
+    carcNumber: '2424242424242424'
+  });
+  console.log(data);
 };
 start();
